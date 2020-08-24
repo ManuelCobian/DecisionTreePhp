c72fac92ffa1aad63170e7195cfd8e4c7c823f01
ManuelCobian <cobian_88@hotmail.com>
2020-08-24 14:41:54 -0500

    add classes traning tree

---
+++ app/core/Tree.php
@@ -0,0 +1,17 @@
+<?php
+namespace core;
+class Tree {
+	protected	$root;
+	private		$currentNode;
+	
+	public function __construct($root) {
+		$this->root = $root;
+	}
+	
+	public function display() {
+		$this->root->display(0);
+	}
+}
+
+
+?>
